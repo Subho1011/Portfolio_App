5ef5c3a623b0c92a50fce4a2155250ea588038a3
Subhabrata Ghoshal <ghoshalsubhabrata683@gmail.com>
2024-05-08 01:59:43 +0530

    Basic structure of Navbar is created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import './App.css';
-import Navbar from './components/Navbar';
+import NavBar from './components/Navbar';
 import BringToTop from './components/BringToTop';
 import PreLoader from './components/PreLoader';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [load, setLoad] = useState(true);
@@ -18,23 +19,24 @@ function App() {
       setLoad(false);
     }, 2000);
 
-    return () => { clearTimeout(timer)};
+    return () => { clearTimeout(timer) };
   }, []);
 
   return (
-    <div>
-      <PreLoader isLoading={load}/>
+    <Router>
+      <PreLoader isLoading={load} />
       <div>
-        <Navbar />
+        <NavBar />
         <BringToTop />
-        <div>
-          <Home />
-          <About />
-          <Projects />
-          <Resume />
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>}/>
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
 
-const Navbar = () => {
-  return(
-    <div>Hello from Navbar</div>
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+   
+        <Navbar.Brand href="/">Brand</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Item> <Nav.Link href="/">Home</Nav.Link> </Nav.Item>
+            <Nav.Item> <Nav.Link href="/about">About</Nav.Link> </Nav.Item>
+            <Nav.Item> <Nav.Link href="//projects">Projects</Nav.Link> </Nav.Item>
+            <Nav.Item> <Nav.Link href="/resume">Resume</Nav.Link> </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+    
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavBar;
