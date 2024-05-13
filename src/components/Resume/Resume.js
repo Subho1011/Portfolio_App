3e04d0f5999ca25507c568e4fbe0c80b0152fc52
Subhabrata Ghoshal <ghoshalsubhabrata683@gmail.com>
2024-05-14 00:46:12 +0530

    Basic structure of Resume page is created

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import myCV from '../../Assests/pdfs/CV_SG.pdf'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
 
 const Resume = () => {
+  const [numPages, setNumPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onLoadHandler(numPages) {
+    setNumPages(numPages);
+  }
+
   return (
-    <div>Hello from Resume</div>
+    <div>
+      <Document file={myCV} onLoadSuccess={onLoadHandler}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of 1</p>
+    </div>
   );
 }
 
